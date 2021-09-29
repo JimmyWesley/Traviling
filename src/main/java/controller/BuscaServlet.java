911cc466d13adeb40b5034aa5a5012378d00de4a
Jimmy Wesley Maciel <rededown@hotmail.com>
2021-09-28 22:52:52 -0300

    Sample Json anexo

--- src/main/java/controller/BuscaServlet.java
+++ src/main/java/controller/BuscaServlet.java
@@ -22,11 +22,9 @@ public class BuscaServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response ) 
 			throws IOException, ServletException 
-	{
-		URI uri = URI.create("http://localhost/path");
-		
+	{		
 		HttpRequest req = HttpRequest.newBuilder()
-				.uri(URI.create("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=pt-br"))
+				.uri(URI.create("https://booking-com.p.rapidapi.com/v1/hotels/locations?name="+request.getParameter("local")+"&locale=pt-br"))
 				.header("x-rapidapi-host", "booking-com.p.rapidapi.com")
 				.header("x-rapidapi-key", "fb8d9b1f46msh43e2e951aadc919p1c610ajsn17111f74430f")
 				.method("GET", HttpRequest.BodyPublishers.noBody())
