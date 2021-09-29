81208b757f3847522a5ad2607100f441408e9735
Jimmy Wesley Maciel <rededown@hotmail.com>
2021-09-28 22:19:00 -0300

    API Ajustes

--- src/main/java/controller/BuscaServlet.java
+++ src/main/java/controller/BuscaServlet.java
@@ -2,6 +2,10 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -19,17 +23,34 @@ public class BuscaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,HttpServletResponse response ) 
 			throws IOException, ServletException 
 	{
+		URI uri = URI.create("http://localhost/path");
+		
+		HttpRequest req = HttpRequest.newBuilder()
+				.uri(URI.create("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=pt-br"))
+				.header("x-rapidapi-host", "booking-com.p.rapidapi.com")
+				.header("x-rapidapi-key", "fb8d9b1f46msh43e2e951aadc919p1c610ajsn17111f74430f")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		
+		HttpResponse<String> resp;
+		
+		try {
+			resp = HttpClient.newHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
+									
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		ArrayList<Hotel> bd = new ArrayList<Hotel>();
 		
 		bd.add(new Hotel(1,"IBIS - SÃO PAULO","Hotel 3 estrellas, com piscina"));
 		bd.add(new Hotel(2,"IBIS - JANDIRA","Hotel 4 estrellas, com CAFÉ"));
 		bd.add(new Hotel(3,"IBIS - OSASCO","Hotel 5 estrellas, com almoço"));
 		
-		//Buscando ID
 		int id = Integer.parseInt(request.getParameter("id"));
-		
-		//Logica para busca
-		
+
  		Hotel result = null;
 		
 		for(Hotel hotel : bd) {
@@ -48,13 +69,11 @@ public class BuscaServlet extends HttpServlet {
 			pgDestino = "/erro.jsp";
 		}
 		
+		request.setAttribute("Hotel", result);
 		
 		RequestDispatcher dispatcher = getServletContext()
 				.getRequestDispatcher(pgDestino);
-		dispatcher.forward(request, response);
-		
-		  PrintWriter out = response.getWriter();	  
-	        out.print(result);
+		dispatcher.forward(request, response);		
 		
 	}
 }
--- webcontent/hoteis.jsp
+++ webcontent/hoteis.jsp
@@ -1,17 +1,20 @@
+<%@page import="model.Hotel"%>
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
-<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="f" %>     
-<jsp:useBean id="Hotel" scope="request" type="model.Hotel"></jsp:useBean>
-<html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="f" %> 
+  
+<jsp:useBean id="Hotel" type="model.Hotel" scope="request" />
+
+<html>                                   
 <head>
 <meta charset="UTF-8">
 <title>.: Sucesso :.</title>
 </head>
-<body>
- <h1> Usuario encontrado com sucesso! </h1>
+<body> 
+ <h1> Hotel encontrado com sucesso! </h1>
  <hr> 
       ID: <c:out value="${Hotel.id}"/> <br>
       Nome: <c:out value="${Hotel.nome}"/> <br>
-      Email: <c:out value="${Hotel.descricao}"/> <br> 
+      Descrição: <c:out value="${Hotel.descricao}"/> <br> 
 </body>
 </html>
